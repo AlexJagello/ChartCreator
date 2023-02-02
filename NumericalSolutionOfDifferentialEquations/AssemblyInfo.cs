81980851d7c6b12d47f2756b9527e0d1c5f7bed1
AlexJ <sascha.yagello@yandex.ru>
2023-02-03 00:16:35 +0300

    Change assembly info file

--- NumericalSolutionOfDifferentialEquations/AssemblyInfo.cs
+++ NumericalSolutionOfDifferentialEquations/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Reflection;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -8,3 +9,13 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
+
+[assembly: AssemblyTitle("Chart creator app")]
+[assembly: AssemblyDescription("This app allow to plot chart, to solve numerically some differential equations")]
+[assembly: AssemblyCompany("AlexJagello")]
+[assembly: AssemblyProduct("AlexJagello (R) Chart creator")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
